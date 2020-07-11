8194e5f080f00ad54a84c90901bd1fcd87f450ec
Gleb Solyanik <46048691+glebsolyanik@users.noreply.github.com>
2020-07-11 12:31:48 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ from selenium import webdriver
 from pprint import pprint
 import time
 from pymongo import MongoClient
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 from main_variables import *
 
 #               Вход в mail
@@ -33,6 +35,7 @@ write_password.send_keys('NextPassword172')
 # кнопка ввод
 input_button = driver.find_element_by_id('mailbox:submit')
 input_button.click()
+#time.sleep(5)
 
 # --------------------------------------------------------------
 
@@ -40,60 +43,44 @@ input_button.click()
 
 letters = []
 
-time.sleep(10)
-letter = driver.find_elements_by_xpath("//div[@class='dataset__items']/a")
-links = []
-for lett in letter:
-    link_to_letter = lett.get_attribute('href')
-    links.append(link_to_letter)
-
-for link in links[16:17]:
-
-    letter_data = {}
-
-    # переходим по ссылке письма
-    driver.get(link)
-
-    time.sleep(10)
-
-    # запись названия темы письма
-    letter_name = driver.find_element_by_xpath("//h2")
-    letter_name = letter_name.text
-    letter_data['name'] = letter_name
-
-    # полный текст письма
-    letter_text = driver.find_element_by_xpath("//div[@class='letter__body']")
-    letter_data['text'] = str(letter_text.text).replace(u"\n", u'')
-
-    # время
-    time_data = driver.find_element_by_xpath("//div[@class='letter__date']")
-    letter_data['time'] = time_data.text
-
-    # запись данных в список
-    letters.append(letter_data)
-    #pprint(letters)
-
-    time.sleep(5)
-
-client = MongoClient('localhost', 27017)
-
-db = client['letters_fom_mailru']
-
-mails = db.letters
-
-mails.drop()
-mails.insert_many(letters)
-
-for mail in mails:
-    pprint(mail)
+link = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, "//a[@class='llc js-tooltip-direction_letter-bottom js-letter-list-item llc_pony-mode llc_normal']"))
+)
+link = link.get_attribute('href')
+driver.get(link)
 
 
+button_down = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, "svg.ico.ico_16-arrow-down.ico_size_s"))
+)
+try:
+    while button_down:
 
+        letter_data = {}
+        # запись названия темы письма
+        letter_name = driver.find_element_by_xpath("//h2")
+        letter_name = letter_name.text
+        letter_data['name'] = letter_name
 
+        # полный текст письма
+        letter_text = driver.find_element_by_xpath("//div[@class='letter__body']")
+        letter_data['text'] = str(letter_text.text).replace(u"\n", u'')
 
+        # время
+        time_data = driver.find_element_by_xpath("//div[@class='letter__date']")
+        letter_data['time'] = time_data.text
 
+        # запись данных в список
+        letters.append(letter_data)
+        pprint(letters)
+        button_down.click()
+        time.sleep(2)
+except:
+    client = MongoClient('localhost', 27017)
 
+    db = client['letters_fom_mailru']
 
+    mails = db.letters
 
-
-
+    mails.drop()
+    mails.insert_many(letters)
